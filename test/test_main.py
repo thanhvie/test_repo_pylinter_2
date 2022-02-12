9dc9feefd38fe676935babca552e15dd5896a8b9
Huynh Thanh Viet <huynhthviet@gmail.com>
2022-02-12 17:39:25 +0700

    change file name

--- test/test_main.py
+++ test/test_main.py
@@ -1,14 +1,14 @@
-import main
+import app
 
 
 def test_add():
-    assert main.add(3, 4) == 7
-    assert main.add(3.5, 4) == 7
-    assert main.add(3.9, 4) == 7
-    assert main.add(3.9, 4.1) == 8
+    assert app.add(3, 4) == 7
+    assert app.add(3.5, 4) == 7
+    assert app.add(3.9, 4) == 7
+    assert app.add(3.9, 4.1) == 8
 
 
 def test_to_sentence():
-    assert main.to_sentence('apple') == 'Apple.'
-    assert main.to_sentence('Apple trees') == 'Apple trees.'
-    assert main.to_sentence('Apple trees.') == 'Apple trees.'
+    assert app.to_sentence('apple') == 'Apple.'
+    assert app.to_sentence('Apple trees') == 'Apple trees.'
+    assert app.to_sentence('Apple trees.') == 'Apple trees.'
